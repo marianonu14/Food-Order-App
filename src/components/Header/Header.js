2747d49b39264f6dfb51e786c4543cf8acfa883a
Mariano <marianonunezma@gmail.com>
2022-07-26 08:50:32 -0300

    First Commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import './Header.css'
+
+const Header = () => {
+    return ( 
+    <header className="header">
+        <nav>
+            <ul>
+            <li>Food Order App</li>
+            <li><button>Log In</button></li>
+            </ul>
+        </nav>
+    </header>
+     );
+}
+ 
+export default Header;
